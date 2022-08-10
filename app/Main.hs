63d4902ccf285b3aca68fe1e80f75dfe92db7058
Hiromi ISHII <konn.jinro@gmail.com>
2022-08-10 23:11:35 +0900

    slightly pretty

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
-module Main where
+module Main (main) where
 
 import Control.Monad (forever)
+import Data.Text (Text)
 import qualified Data.Text.IO as T
 import Language.Lambda.Syntax.LambdaPi (Type, Value, tryEval)
 import Language.Lambda.Syntax.LambdaPi.Parser
@@ -11,14 +12,18 @@ main = forever $ do
   putStr ">>> "
   hFlush stdout
   src <- T.getLine
-  either report reportResult $
+  either reportError (printResult src) $
     tryEval =<< parseOnly lambdaExp src
 
-reportResult :: (Value, Type) -> IO ()
-reportResult (val, ty) =
-  print val >> putStrLn ("\t:: " <> show ty)
+printResult :: Text -> (Value, Type) -> IO ()
+printResult inp (val, ty) = do
+  T.putStrLn inp
+  putStr "\t= "
+  print val
+  putStr "\t:: "
+  print ty
 
-report :: String -> IO ()
-report err = do
+reportError :: String -> IO ()
+reportError err = do
   hPutStrLn stderr "Compile error:\n"
   hPutStrLn stderr err
