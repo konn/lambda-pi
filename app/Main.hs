afde83ce7ef19df581b1918787fa4b3c0cd7496e
Hiromi ISHII <konn.jinro@gmail.com>
2022-08-10 23:04:49 +0900

    feat: hypersimple  REPL

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,18 @@
 module Main where
 
+import Control.Monad (forever)
+import qualified Data.Text.IO as T
+import Language.Lambda.Syntax.LambdaPi (tryEval)
+import Language.Lambda.Syntax.LambdaPi.Parser
+import System.IO (hPutStr, hPutStrLn, stderr)
+
 main :: IO ()
-main = pure ()
+main = forever $ do
+  src <- T.getLine
+  either report print $
+    tryEval =<< parseOnly lambdaExp src
+
+report :: String -> IO ()
+report err = do
+  hPutStrLn stderr "Compile error:\n"
+  hPutStrLn stderr err
