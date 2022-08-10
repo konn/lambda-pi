217a945b43d7310f82dc13bc35ba707ce3341f8c
Hiromi ISHII <konn.jinro@gmail.com>
2022-08-10 23:06:32 +0900

    fix: prompting

--- app/Main.hs
+++ app/Main.hs
@@ -2,15 +2,21 @@ module Main where
 
 import Control.Monad (forever)
 import qualified Data.Text.IO as T
-import Language.Lambda.Syntax.LambdaPi (tryEval)
+import Language.Lambda.Syntax.LambdaPi (Type, Value, tryEval)
 import Language.Lambda.Syntax.LambdaPi.Parser
-import System.IO (hPutStr, hPutStrLn, stderr)
+import System.IO (hFlush, hPutStrLn, stderr, stdout)
 
 main :: IO ()
 main = forever $ do
+  putStr ">>> "
+  hFlush stdout
   src <- T.getLine
-  either report print $
+  either report reportResult $
     tryEval =<< parseOnly lambdaExp src
+
+reportResult :: (Value, Type) -> IO ()
+reportResult (val, ty) =
+  print val >> putStrLn ("\t:: " <> show ty)
 
 report :: String -> IO ()
 report err = do
