2e84e12a3b5018bca8b282de253a0df7b8b1c58a
Hiromi ISHII <konn.jinro@gmail.com>
2023-06-04 15:11:38 +0900

    enrich REPL

--- app/Main.hs
+++ app/Main.hs
@@ -2,14 +2,23 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Main (main) where
 
+import Control.Lens
+import Control.Monad.Loops (whileJust_)
 import Data.Generics.Labels ()
+import qualified Data.HashMap.Strict as HM
+import Data.List (sort)
 import qualified Data.Text as T
-import qualified Data.Text.IO as T
+import Language.Lambda.Syntax.LambdaPi.Parser (keywords)
 import Language.Lambda.Syntax.LambdaPi.REPL
 import RIO
+import RIO.Directory (getHomeDirectory)
+import RIO.FilePath ((</>))
+import RIO.Orphans ()
+import System.Console.Haskeline
 
 data AppEnv = AppEnv {logFun :: LogFunc, envRef :: SomeRef REPLContext}
   deriving (Generic)
@@ -22,12 +31,36 @@ instance HasStateRef REPLContext AppEnv where
   stateRefL = #envRef
   {-# INLINE stateRefL #-}
 
+appLineSettings :: RIO AppEnv (Settings (RIO AppEnv))
+appLineSettings = do
+  home <- getHomeDirectory
+  pure
+    (defaultSettings @(RIO AppEnv))
+      { complete = completeWord Nothing " \n\r\t" $ \inp -> do
+          keys <- uses #bindings $ sort . (toList keywords <>) . HM.keys
+          let comps =
+                [ Completion
+                  { replacement = T.unpack candidate
+                  , isFinished = True
+                  , display = T.unpack candidate
+                  }
+                | candidate <- keys
+                , T.pack inp `T.isPrefixOf` candidate
+                ]
+          pure comps
+      , historyFile = Just $ home </> ".lambda-pi-history"
+      }
+
 main :: IO ()
 main = do
   logOpts <- logOptionsHandle stdout False
   envRef <- newSomeRef mempty
   withLogFunc logOpts $ \logFun ->
-    runRIO AppEnv {..} $
-      forever $ do
-        liftIO $ putStr ">>> " >> hFlush stdout
-        liftIO T.getLine >>= readEvalPrintM . T.strip
+    runRIO AppEnv {..} $ do
+      sett <- appLineSettings
+      runInputT sett mainLoop `finally` liftIO (putStrLn "Good bye.")
+  where
+    mainLoop = withInterrupt $ handleInterrupt mainLoop $ do
+      whileJust_ (getInputLine ">>> ") $ \inp ->
+        handleInterrupt (outputStrLn "^CInterrupted." *> mainLoop) $ do
+          lift $ readEvalPrintM $ T.pack inp
