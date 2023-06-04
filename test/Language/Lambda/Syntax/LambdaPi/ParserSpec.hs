7c10cc3f0d0bc35fdc5140fc1a45bd551c6d23b4
Hiromi ISHII <konn.jinro@gmail.com>
2023-06-04 19:23:07 +0900

    tests: more and more cases

--- test/Language/Lambda/Syntax/LambdaPi/ParserSpec.hs
+++ test/Language/Lambda/Syntax/LambdaPi/ParserSpec.hs
@@ -1,10 +1,29 @@
+{-# LANGUAGE DataKinds #-}
+
 module Language.Lambda.Syntax.LambdaPi.ParserSpec where
 
+import qualified Data.Bifunctor as Bi
 import qualified Data.Text as T
 import Language.Lambda.Syntax.LambdaPi
 import Language.Lambda.Syntax.LambdaPi.Parser
 import Test.Tasty
 import Test.Tasty.HUnit
+
+inferableCases :: [(String, Term 'Inferable)]
+inferableCases =
+  [ ("Nat -> Nat", Pi (Inf Nat) $ Inf Nat)
+  , ("(Nat -> Nat)", Pi (Inf Nat) $ Inf Nat)
+  , ("Nat", Nat)
+  , ("Nat -> Nat -> Nat", Pi (Inf Nat) $ Inf $ Pi (Inf Nat) (Inf Nat))
+  , ("Nat -> (Nat -> Nat)", Pi (Inf Nat) $ Inf $ Pi (Inf Nat) (Inf Nat))
+  , ("(Nat -> Nat -> Nat)", Pi (Inf Nat) $ Inf $ Pi (Inf Nat) (Inf Nat))
+  , ("(Nat -> (Nat -> Nat))", Pi (Inf Nat) $ Inf $ Pi (Inf Nat) (Inf Nat))
+  , ("(Nat -> Nat) -> Nat", Pi (Inf $ Pi (Inf Nat) (Inf Nat)) $ Inf Nat)
+  , ("((Nat -> Nat) -> Nat)", Pi (Inf $ Pi (Inf Nat) (Inf Nat)) $ Inf Nat)
+  , ("Nat", Nat)
+  , ("(Nat)", Nat)
+  , ("((Nat))", Nat)
+  ]
 
 test_checkableExprP :: TestTree
 test_checkableExprP =
@@ -14,12 +33,18 @@ test_checkableExprP =
         "Regression Test"
         [ testCase src $
           parseOnly checkableExprP (T.pack src) @?= Right expect
-        | (src, expect) <-
-            [ ("Nat -> Nat", Inf $ Pi (Inf Nat) $ Inf Nat)
-            , ("(Nat -> Nat)", Inf $ Pi (Inf Nat) $ Inf Nat)
-            , ("Nat", Inf Nat)
-            , ("(Nat)", Inf Nat)
-            , ("((Nat))", Inf Nat)
-            ]
+        | (src, expect) <- map (Bi.second Inf) inferableCases
+        ]
+    ]
+
+test_inferableExprP :: TestTree
+test_inferableExprP =
+  testGroup
+    "inferableExprP"
+    [ testGroup
+        "Regression Test"
+        [ testCase src $
+          parseOnly inferableExprP (T.pack src) @?= Right expect
+        | (src, expect) <- inferableCases
         ]
     ]
