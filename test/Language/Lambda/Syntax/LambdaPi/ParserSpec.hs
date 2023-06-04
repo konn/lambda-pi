2e4fc31cf4fdef7f7f0df6af67cfc21965d6a7d3
Hiromi ISHII <konn.jinro@gmail.com>
2023-06-04 22:05:03 +0900

    test: More and more cases

--- test/Language/Lambda/Syntax/LambdaPi/ParserSpec.hs
+++ test/Language/Lambda/Syntax/LambdaPi/ParserSpec.hs
@@ -24,13 +24,16 @@ inferableCases =
   , ("(Nat -> Nat) -> Nat", Pi (Inf $ Pi (Inf Nat) (Inf Nat)) $ Inf Nat)
   , ("((Nat -> Nat) -> Nat)", Pi (Inf $ Pi (Inf Nat) (Inf Nat)) $ Inf Nat)
   , ("natElim", LamAnn (Inf (Pi (Inf Nat) (Inf Star))) (LamAnn (Inf (Bound 0 :@: Inf Zero)) (LamAnn (Inf (Pi (Inf Nat) (Inf (Pi (Inf (Bound 2 :@: Inf (Bound 0))) (Inf (Bound 3 :@: Inf (Succ (Inf (Bound 1))))))))) (LamAnn (Inf Nat) (NatElim (Inf (Bound 3)) (Inf (Bound 2)) (Inf (Bound 1)) (Inf (Bound 0)))))))
-  ,
-    ( "Vec Nat 5"
-    , LamAnn (Inf Star) (LamAnn (Inf Nat) (Vec (Inf (Bound 1)) (Inf (Bound 0))))
-        :@: Inf Nat
-        :@: Inf (SuccI (SuccI (SuccI (SuccI (SuccI Zero)))))
-    )
+  , ("Vec Nat 5", vecNat5)
+  , ("(Vec Nat) 5", vecNat5)
+  , ("(Vec Nat 5)", vecNat5)
   ]
+
+vecNat5 :: Term 'Inferable
+vecNat5 =
+  LamAnn (Inf Star) (LamAnn (Inf Nat) (Vec (Inf (Bound 1)) (Inf (Bound 0))))
+    :@: Inf Nat
+    :@: Inf (SuccI (SuccI (SuccI (SuccI (SuccI Zero)))))
 
 pattern SuccI :: Term 'Inferable -> Term 'Inferable
 pattern SuccI n = Succ (Inf n)
